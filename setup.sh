0b4eea55a365d1876e0a69cc20ba03a4e049a92d
Amonryan <ryan.em.oliveira@gmail.com>
2025-02-24 00:28:00 -0300

    correção no script

--- setup.sh
+++ setup.sh
@@ -4,21 +4,23 @@
 LOGFILE="/var/log/user-data.log"
 exec > >(tee -a "$LOGFILE") 2>&1
 
+echo "### Instalando Git..."
+sudo yum install -y git
 
-# Clonando repositório do site
+
 echo "### Clonando site do GitHub..."
-git clone https://github.com/amonryan/ativiadeaws.git /var/www/html/
+sudo git clone https://github.com/amonryan/ativiadeaws.git /var/www/html/
 
 
 echo "### Iniciando setup do servidor..."  
-yum -y install httpd
+sudo yum -y install httpd
 
 echo "### Iniciando Apache..."
-systemctl enable httpd
-systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd
 
 
 echo "### Enviando requisição POST..."
-curl -X POST -d "nome=Ryan Emmanuel" https://difusaotech.com.br/lab/aws/index.php
+sudo curl -X POST -d "nome=Ryan Emmanuel" https://difusaotech.com.br/lab/aws/index.php
 
 echo "### Script finalizado!"
